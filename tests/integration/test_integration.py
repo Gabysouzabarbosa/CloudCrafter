afd9571b78a830ad851917040a0d49ec4c5982c2
Gabysouzabarbosa <gaby.desouzabarbosa@gmail.com>
2024-09-26 21:00:37 -0300

    Corrigido o estilo do código conforme flake8

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -4,7 +4,6 @@ import requests
 
 class TestIntegration(unittest.TestCase):
 
-
     def test_api_status(self):
         response = requests.get('http://localhost:8000/')
         self.assertEqual(response.status_code, 200)
