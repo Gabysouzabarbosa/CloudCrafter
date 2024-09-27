e8a8bf0163735068b0f82341e09137d72416c27b
gabysouzabarbosa <gaby.desouzabarbosa@gmail.com>
2024-09-26 22:14:01 -0300

    Corrigido o estilo do código conforme flake8

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -4,10 +4,12 @@ import requests
 
 class TestIntegration(unittest.TestCase):
 
+
     def test_api_status(self):
         response = requests.get('http://localhost:8000/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"status": "ok"})
+
 
 if __name__ == "__main__":
     unittest.main()
